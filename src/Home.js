a083af3398b6976d3cec950e2191c4d027ba0786
HarshSutaria_vsspl <harshrajesh.sutaria@vasudhaika.net>
2022-06-22 13:03:18 +0530

    R[8]

--- src/Home.js
+++ src/Home.js
@@ -1,22 +1,34 @@
+import { useState } from "react"; //importing useState hook
 let Home = () => {
   let homeTitle = `Home Component`;
+
+  let [name, setName] = useState("mario");
+  let [age, setAge] = useState("18");
   // function to handle a click
   const handleClick = () => {
-    console.log("muje kya mai to cheemba huh!!!!!!!");
+    console.log("muje kya mai to cheemba huh tuhuh!!!!!!!");
   };
 
   // function to handle click again
 
-  const handleClickAgain = (name) => {
-    console.log("hello ", name);
+  const handleClickAgain = (name, age) => {
+    // console.log("hello ", name, age);
+    setName(name);
+    setAge(age);
   };
-
+  /**
+   * to immplement reactive valuse we will use useEffect hook
+   */
   return (
     <div>
       <h2>{homeTitle}</h2>
-      //creating a button and mapping it to a function
+      <h1>
+        {name} is {age} years old !!!!!
+      </h1>
       <button onClick={handleClick}> click me bruh !!!!!!!</button>
-      <button onClick={(name) => handleClickAgain("cheembaaaaaaaaa!!!")}>
+      <button
+        onClick={(name, age) => handleClickAgain("cheembaaaaaaaaa!!!", 12)}
+      >
         click me again brumh!!!!!!!!!!!
       </button>
     </div>
